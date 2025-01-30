19ba581ac64082d2fa4bbfb68eafbc692c3d71b6
christopher vos <christophervos522@gmail.com>
2025-01-31 00:08:20 +0200

    added folder controller

--- src/routes/folders.route.js
+++ src/routes/folders.route.js
@@ -1,46 +1,11 @@
 
-const osPath = require("path");
 const { Router } = require("express");
-const { getSettings } = require("../lib/utils.js");
-
-const SETTINGS = getSettings();
+const { GET } =require("../controllers/folders.controller.js")
 
 const folderRouter = Router();
 
 folderRouter
-    .get('/api/folders', async (req,res)=> {
-        const allowed = SETTINGS.allowList;
-
-        let data = allowed.map(filetype=>{
-            let buffer;
-            switch (filetype) {
-                case "audio":
-                    buffer = SETTINGS.audioPaths.map(path=>{
-                        if (path == "./") {
-                            return osPath.basename(process.cwd())
-                        }else {
-                            return osPath.basename(path)
-                        }
-                    })
-                    break;
-                case "image":
-                    buffer = SETTINGS.imagePaths
-                    break
-                default:
-                    break;
-            }
-
-            return {
-                type: filetype,
-                folders: buffer
-            }
-        })
-
-        return res.json({
-            data: data,
-            message: "Successfully fetched folders"
-        })
-    })
+    .get('/api/folders', GET)
 
 
 module.exports = folderRouter
