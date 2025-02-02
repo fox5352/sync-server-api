75adcbe24aeb7ed60e8a6de993aa753fa313d95a
christopher vos <christophervos522@gmail.com>
2025-02-02 16:21:46 +0200

    added missing import on server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const { app } = require("./app.js");
-const { getSettings } = require("./lib/utils.js")
+const { getSettings, getIpAddress } = require("./lib/utils.js")
+
 
 const SETTINGS = getSettings();
 
@@ -8,11 +9,11 @@ const SETTINGS = getSettings();
 
 const server = http.createServer(app);
 
-server.listen(SETTINGS.server.port, SETTINGS.server.host,()=>{
+server.listen(SETTINGS.server.port, SETTINGS.server.host, () => {
     let address = '';
     if (SETTINGS.server.host == "0.0.0.0") {
         address = `http://${getIpAddress()}:${SETTINGS.server.port}`
-    }else {
+    } else {
         address = `http://localhost:${SETTINGS.server.port}`
     }
     console.log(`Server is running on ${address}`);
