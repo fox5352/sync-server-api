13e0b62d3fde56fcc3167a1c5409a3a5d4f4d78c
Christopher vos <christophervos522@gmail.com>
2025-02-16 20:46:55 +0200

    fixed typo

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -1,15 +1,15 @@
-const { getFiles, readFileData, getFileMetadata } = require("../lib/fileManagement1");
+const { getFiles, readFileData, getFileMetadata } = require("../lib/fileManagement");
 const { getSettings } = require("../lib/utils");
 
 const SETTINGS = getSettings();
 
 async function GET(req, res) {
     const filetype = req.params.filetype;
-    const {name, path:queryPath} = req.query
-    
+    const { name, path: queryPath } = req.query
+
 
     if (!name || !queryPath) {
-        res.status(400).json({message: "Invalid request payload requires both name and path in query"})
+        res.status(400).json({ message: "Invalid request payload requires both name and path in query" })
     }
 
     try {
@@ -17,21 +17,21 @@ async function GET(req, res) {
 
         if (!pathsList) {
             throw new Error("failed to get paths list");
-        }        
+        }
 
         for (const path of pathsList) {
             const files = await getFiles(path);
 
             if (files.length == 0) throw new Error("no files found");
-                        
-            for (const file of files) {                
+
+            for (const file of files) {
 
                 if (file.name.toLowerCase().includes(name)) {
 
                     if (file.path == queryPath) {
                         const metaData = await getFileMetadata(file.path, filetype);
                         const fileBuffer = await readFileData(file.path);
-                        
+
                         return res.json({
                             message: "file fetched successfully",
                             data: {
@@ -46,7 +46,7 @@ async function GET(req, res) {
         }
 
 
-        res.status(404).json({message: "file not found"});
+        res.status(404).json({ message: "file not found" });
     } catch (error) {
         res.status(500).json({
             message: `Internal Server Error on attempt ${filetype} fetch in file route`
